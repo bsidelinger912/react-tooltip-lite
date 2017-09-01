1271a6ba764aabda0ef8ff15960f07ce7ee65179
Ben Sidelinger <ben.sidelinger@gmail.com>
2017-08-31 22:15:05 -0700

    Proptypes in portal (#4)
    * use prop-types package in Portal.jsx

--- dist/Portal.js
+++ dist/Portal.js
@@ -10,6 +10,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
@@ -83,6 +87,6 @@ var Portal = function (_React$Component) {
 }(_react2.default.Component);
 
 Portal.propTypes = {
-  children: _react.PropTypes.node.isRequired
+  children: _propTypes2.default.node.isRequired
 };
 exports.default = Portal;
