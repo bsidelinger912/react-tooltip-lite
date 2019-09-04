1781bcaa4e770e5fb7923c6445343d8ad3dedd61
Liam Keily <liamk15@gmail.com>
2019-09-04 11:43:56 +0100

    Allow user to override zIndex

--- src/index.jsx
+++ src/index.jsx
@@ -42,6 +42,7 @@ class Tooltip extends React.Component {
     tipContentClassName: PropTypes.string,
     useDefaultStyles: PropTypes.bool,
     useHover: PropTypes.bool,
+    zIndex: PropTypes.number
   }
 
   static defaultProps = {
@@ -66,6 +67,7 @@ class Tooltip extends React.Component {
     tipContentClassName: undefined,
     useDefaultStyles: false,
     useHover: true,
+    zIndex: 1000
   }
 
   static getDerivedStateFromProps(nextProps) {
@@ -270,7 +272,7 @@ class Tooltip extends React.Component {
         color: useDefaultStyles ? defaultColor : color,
         padding,
         boxSizing: 'border-box',
-        zIndex: 1000,
+        zIndex: this.props.zIndex,
         position: 'absolute',
         display: 'inline-block',
       };
@@ -280,7 +282,7 @@ class Tooltip extends React.Component {
         position: 'absolute',
         width: '0px',
         height: '0px',
-        zIndex: 1001,
+        zIndex: this.props.zIndex + 1,
       };
 
       tipPortal = (
